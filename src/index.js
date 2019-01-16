913d9923a7a5b1bb76922e4fd566e25c9f43c586
Robert Snyder <r.snyder49@gmail.com>
2019-01-16 01:43:17 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -38,9 +38,11 @@ const Login = () => {
 
 ReactDOM.render((
   <Router>
+    <React.Fragment>
       <Route path="/" render={Home} />
       <Route exact path="/about" render={About} />
       <Route exact path="/login" render={Login} />
+    </React.Fragment>
   </Router>),
   document.getElementById('root')
 );
