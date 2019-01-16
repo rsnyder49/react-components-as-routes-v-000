e0a54e85b822ccafc2d753d8ff2c6810eaff39ea
Robert Snyder <r.snyder49@gmail.com>
2019-01-16 01:46:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -11,46 +11,61 @@ const link = {
   color: 'white',
 }
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Home!</h1>
-      </div>
-    )
-  }
-}
-
-const About = () => {
-  return (
+/* add the navbar component */
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>;
+ 
+const Home = () => <h1>Home!</h1>;
+ 
+const About = () => <h1>This is my about component!</h1>;
+ 
+const Login = () =>
+  <form>
+    <h1>Login</h1>
     <div>
-      <h1>This is my about component!</h1>
+      <input type="text" name="username" placeholder="Username" />
+      <label htmlFor="username">Username</label>
     </div>
-  );
-};
-
-const Login = () => {
-  return (
     <div>
-      <form>
-        <div>
-          <input type="text" name="username" placeholder="Username" />
-          <label htmlFor="username">Username</label>
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" />
-          <label htmlFor="password">Password</label>
-        </div>
-        <input type="submit" value="Login" />
-      </form>
+      <input type="password" name="password" placeholder="Password" />
+      <label htmlFor="password">Password</label>
     </div>
-  );
-};
-
+    <input type="submit" value="Login" />
+  </form>;
+ 
 ReactDOM.render((
   <Router>
     <React.Fragment>
-      <Route path="/" component={Home} />
+      <Navbar />
+      <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/login" component={Login} />
     </React.Fragment>
