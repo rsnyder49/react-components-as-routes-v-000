fb87627017e54e76c6e3fe26bd046aab42c9993b
Robert Snyder <r.snyder49@gmail.com>
2019-01-16 01:41:55 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -18,6 +18,24 @@ const About = () => {
   );
 };
 
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
+
 ReactDOM.render((
   <Router>
     <Route path="/" render={Home} />
