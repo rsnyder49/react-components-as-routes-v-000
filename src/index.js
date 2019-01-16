20c102c1befaa8714432fccfd170afe8cf3f32e3
Robert Snyder <r.snyder49@gmail.com>
2019-01-16 01:44:13 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Home!</h1>
+      </div>
+    )
+  }
+}
 
 const About = () => {
   return (
